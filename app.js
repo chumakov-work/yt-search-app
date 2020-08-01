d1085879581b12216ce97f51bb81c25c356de63a
Nikolay Chumakov <nklchumakov@gmail.com>
2020-08-01 14:30:13 +0300

    v1.3

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const API_KEY = 'AIzaSyDQFs7fryLROnupgmBSO-vX4PL23jcj8yM'
+//
+
+const API_KEY = 'AIzaSyBvJF0nvvfSbKH1_6VdN08xNX-cFbhfzQ4'
+const MAX_RESULTS = 10
+
 const searchBtn = document.getElementById('searchBtn')
 const searchInput = document.getElementById('search')
 const output = document.getElementById('output')
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-})
-
-searchBtn.addEventListener('click', (event) => {
+searchBtn.addEventListener('click', () => {
     var searching = searchInput.value
-    videoSearch(API_KEY, 10, searching)
+    videoSearch(API_KEY, MAX_RESULTS, searching)
     output.innerHTML = ''
 })
 
+//получение списка самых новых видеороликов
 function videoSearch(key, maxResults, search) {
     const promise = new Promise((resolve, reject) => {
         if (searchInput.value !== '') {
-            let requestUrl = ('https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet&key=' + key + '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search)
+            let requestUrl = requestLink(key, maxResults, search)
+            console.log('Запрос самых новых видеороликов...')
             const xhr = new XMLHttpRequest()
             xhr.open('GET', requestUrl, true)
             xhr.responseType = 'json'
@@ -31,50 +33,114 @@ function videoSearch(key, maxResults, search) {
     })
 
     promise.then(data => {
-        output.innerHTML = `<p class="result">Результат поиска по запросу: <b><span id="resultWord">${searchInput.value}</span></b></p>`
+        output.innerHTML = searchWord(searchInput.value)
+        let index = 0
+        let containers = new Array()
+        let views = new Array()
 
+        // получение количества просмотров для каждого видеоролика
+        // Отображаются только заверщенные трансляции и премьеры
         data.items.forEach(video => {
-            let videoTitle = video.snippet.title
-            let author = video.snippet.channelTitle
+            const promiseStats = new Promise((resolve, reject) => {
+                let statUrl = statisticsLink(video.id.videoId)
+                console.log('Получение количества просмотров для видео с id:', video.id.videoId)
+                const xhrStats = new XMLHttpRequest()
+                xhrStats.open('GET', statUrl, true)
+                xhrStats.responseType = 'json'
+                xhrStats.onload = () => {
+                    (xhrStats.status >= 400) ? console.error(xhrStats.response) : resolve(xhrStats.response)
+                }
+                xhrStats.onerror = () => {
+                    console.log(xhrStats.response)
+                }
+                xhrStats.send()
+            })
 
-            let element = `
-            <div class="element mb-3" style="background-color: #ededed">
-                <div class="elementInner d-flex justify-content-start align-items-center">
-                    <img src="${video.snippet.thumbnails.medium.url}" alt="preview" class="preview m-3" style="width: 160px">
-                    <div class="elementDesc w-75">
-                        <h3 class="videoTitle">${(videoTitle.length > 60) ? videoTitle.slice(0, 60) + '...' : videoTitle}</h3>
-                        <p id="author">Автор: ${(author.length > 20) ? author.slice(0, 20) + '...' : author}</p>
-                    </div>
-                    <div class="videoInfo d-flex flex-column justify-content-center">
-                        <p id="videoDate">Дата публикации:</p>
-                        <p id="videoDate">${video.snippet.publishedAt}</p>
-                    </div>
-                </div>
-
-                <div class="videoPlayer hidden w-100 m-3">
-                    <iframe id="ytplayer" class="mx-auto" type="text/html" width="97.5%" height="600" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allowfullscreen></iframe>
-                </div>
-            </div>
-            ` 
-
-            output.innerHTML += element
-            searchInput.value = ''
-
-            let title = document.querySelectorAll('.videoTitle')
-            console.log(title)
-            let elements = document.querySelectorAll('.element')
-            for (let i=0; i<title.length; i++) {
-                title[i].addEventListener('click', () => {
-                    for (let y=0; y<title.length; y++) {
-                        elements[y].querySelector('.videoPlayer').classList.add('hidden')
-                    }
-                    elements[i].querySelector('.videoPlayer').classList.toggle('hidden');
-
-                    title[i].addEventListener('click', () => {
-                        elements[i].querySelector('.videoPlayer').classList.toggle('hidden')
-                    })
+            // сортировка самых новых видеороликов по кол-ву просмотров
+            promiseStats.then(data => {
+                index++
+                let viewCount = data.items[0].statistics.viewCount
+                
+                containers.push({
+                    videos: video,
+                    viewCounts: viewCount
                 })
-            }
+                views.push(viewCount)
+                
+                if (index == maxResults) {
+                    views.sort(compare)
+                    for (let viewsCounter=0; viewsCounter<views.length; viewsCounter++) {
+                        for (let containerCounter=0; containerCounter<containers.length; containerCounter++) {
+                            if(containers[containerCounter].viewCounts == views[viewsCounter]) {
+                                videoElement(containers[containerCounter].videos)
+                                console.log('Добавлен новый элемент к списку видео с порядковым номером:', viewsCounter)
+                            }
+                        }
+                    } 
+                }
+            })
         }) 
     }) 
 }
+
+function compare(a, b) {
+    return b - a;
+}
+
+function videoElement (array) {
+    let videoTitle = array.snippet.title
+    let author = array.snippet.channelTitle
+
+    let element = `
+        <div class="element mb-3" style="background-color: #ededed">
+            <div class="elementInner d-flex justify-content-start align-items-center">
+                <img src="${array.snippet.thumbnails.medium.url}" alt="preview" class="preview m-3" style="width: 160px">
+                <div class="elementDesc w-75">
+                    <h3 class="videoTitle">${(videoTitle.length > 60) ? videoTitle.slice(0, 60) + '...' : videoTitle}</h3>
+                    <p id="author">Автор: ${(author.length > 40) ? author.slice(0, 40) + '...' : author}</p>
+                </div>
+                <div class="videoInfo d-flex flex-column justify-content-center">
+                    <p id="videoDate">Дата публикации:</p>
+                    <p id="videoDate">${new Date(array.snippet.publishedAt)}</p>
+                </div>
+            </div>
+            <div class="videoPlayer hidden w-100 m-3">
+                <iframe id="ytplayer" class="mx-auto" type="text/html" width="97.5%" height="600" src="https://www.youtube.com/embed/${array.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    ` 
+
+    output.innerHTML += element
+    searchInput.value = ''
+
+    let title = document.querySelectorAll('.videoTitle')
+    let elements = document.querySelectorAll('.element')
+
+    for (let i=0; i<title.length; i++) {
+        title[i].addEventListener('click', () => {
+            for (let y=0; y<title.length; y++) {
+                elements[y].querySelector('.videoPlayer').classList.add('hidden')
+            }
+            elements[i].querySelector('.videoPlayer').classList.toggle('hidden');
+            title[i].addEventListener('click', () => {
+                elements[i].querySelector('.videoPlayer').classList.toggle('hidden')
+            })
+        })
+    }
+}
+
+function searchWord (search) {
+    return `<p class="result">Результат поиска по запросу: <b><span id="resultWord">${search}</span></b></p>`
+}
+
+function requestLink (key, maxResults, search) {
+    return ('https://www.googleapis.com/youtube/v3/search??eventType=completed&order=date&part=snippet&key=' + key + '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search)
+}
+
+function statisticsLink (videoId) {
+    return (`https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${videoId}&fields=items%2Fstatistics(viewCount)&key=${API_KEY}`)
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
